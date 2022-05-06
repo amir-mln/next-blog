6aafe609ba2be5b63718fc23a674985e946a4486
amir-mln <devcareer.me@gmail.com>
2022-05-06 18:43:57 +0430

    changed folder structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
 // fixes a bug for next-auth and mongodb atlas somehow
 // https://github.com/nextauthjs/next-auth/issues/833
-import 'reflect-metadata'
-import React from 'react'
-import { SessionProvider } from 'next-auth/react'
-import '../styles/globals.css'
+import 'reflect-metadata';
+import React from 'react';
+import { SessionProvider } from 'next-auth/react';
+import 'styles/globals.css';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;

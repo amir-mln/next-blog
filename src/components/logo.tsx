89583e7615f161d2123339bf7e49e57326ff3c4b
amir-mln <devcareer.me@gmail.com>
2022-05-07 09:30:00 +0430

    changed the logo to my own prefrence

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Link from 'next/link'
-import { Text } from 'evergreen-ui'
+import React from 'react';
+import Link from 'next/link';
+import { Text } from 'evergreen-ui';
 
 const Logo = ({ ...styles }) => {
   return (
     <Link href="/">
       <a>
-        <Text fontSize="30px" color="#47B881" {...styles}>
-          <strong>Known.</strong>
+        <Text fontSize="30px" color="#47B881" fontStyle="italic" {...styles}>
+          <strong>Next Blog!</strong>
         </Text>
       </a>
     </Link>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
